8246b411b3c6efefb619c84f37e2100ad42942d6
EC2 Default User <ec2-user@ip-172-31-50-209.ec2.internal>
2023-05-25 20:04:20 +0000

    update

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ class Main:
     answer = 0
     starttme = time.time()
     timeduration = 5
+    attempts = 0
+    counter = 0    
+   
     
-    def addition(self):
-        attempts = 0
-        counter = 0
+    while counter<=40:
         while counter<=10:
             first = random.randint(1,10)
             second = random.randint(1,10)
@@ -25,8 +26,45 @@ class Main:
                 print("That is not the right answer. Try again")
                 attempts += 1
                 break
+            if counter == 10:
+                while counter <=19:
+                    first = random.randint(1,10)
+                    second = random.randint(1,10)
+                    answer = first - second
+                    answer_input = input(str(first) + " - " + str(second) +": ")
+                    if str(answer) == answer_input:
+                        counter += 1
+                    else:
+                        print("That is not the right answer. Try again")
+                        attempts += 1
+                        break
+                    if counter == 19:
+                        while counter <=28:
+                            first = random.randint(1,10)
+                            second = random.randint(1,10)
+                            answer = first * second
+                            answer_input = input(str(first) + " x " + str(second) +": ")
+                            if str(answer) == answer_input:
+                                counter += 1
+                            else:
+                                print("That is not the right answer. Try again")
+                                attempts += 1
+                                break
+                            if counter == 28:
+                                while counter <=37:
+                                    first = random.randint(1,10)
+                                second = random.randint(1,10)
+                                answer = first % second
+                                answer_input = input(str(first) + " % " + str(second) +": ")
+                                if str(answer) == answer_input:
+                                    counter += 1
+                                else:
+                                    print("That is not the right answer. Try again")
+                                    attempts += 1
+                                    break
+                                if counter == 37:
+                                    print("You've won, congrats!")
+            
+  
     
-    def timer5(self):
-        
-            
-            
+  
